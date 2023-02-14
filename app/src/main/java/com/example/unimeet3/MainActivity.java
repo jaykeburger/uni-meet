efcdea19ca67899bb5f5ddc0958e16c2134b6ed6
Jake <jaykegamer12@gmail.com>
2023-02-13 19:11:39 -0600

    added basic authentication

--- app/src/main/java/com/example/unimeet3/MainActivity.java
+++ app/src/main/java/com/example/unimeet3/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.unimeet3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView username = (TextView) findViewById(R.id.username); //instance of textview
+        TextView password = (TextView) findViewById(R.id.password); //instance of password
+
+        MaterialButton login_button = (MaterialButton) findViewById(R.id.login_button); //instance of login_button
+
+        login_button.setOnClickListener(new View.OnClickListener() { //creates click listener that will check if login_button is clicked
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show(); //login successful will pop up
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"LOGIN FAILED", Toast.LENGTH_SHORT).show(); //login failed will pop up
+                }
+            }
+        });
     }
 }
