33cb166e2358f3d2f9cfa2075c3cdb538c1bfb63
Jake <jaykegamer12@gmail.com>
2023-02-13 19:19:04 -0600

    added redirection template

--- app/src/main/java/com/example/unimeet3/MainActivity.java
+++ app/src/main/java/com/example/unimeet3/MainActivity.java
@@ -18,18 +18,40 @@ public class MainActivity extends AppCompatActivity {
 
         TextView username = (TextView) findViewById(R.id.username); //instance of textview
         TextView password = (TextView) findViewById(R.id.password); //instance of password
+        TextView new_user = (TextView) findViewById(R.id.newuser); //instance of newuser
+        TextView forgot_password = (TextView) findViewById(R.id.forgotpassword); //instance of forgotpassword
 
         MaterialButton login_button = (MaterialButton) findViewById(R.id.login_button); //instance of login_button
+
+        String admin_username = "admin"; //temporary username fields, will check database for others
+        String admin_password = "admin";
 
         login_button.setOnClickListener(new View.OnClickListener() { //creates click listener that will check if login_button is clicked
             @Override
             public void onClick(View view) {
-                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                if(username.getText().toString().equals(admin_username) && password.getText().toString().equals(admin_password)) {
                     Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show(); //login successful will pop up
+                    //TODO: REDIRECT TO HOME PAGE
                 }
                 else {
                     Toast.makeText(MainActivity.this,"LOGIN FAILED", Toast.LENGTH_SHORT).show(); //login failed will pop up
                 }
+            }
+        });
+
+        new_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"CREATE ACCOUNT", Toast.LENGTH_SHORT).show();
+                //TODO: REDIRECT TO SIGN UP PAGE
+            }
+        });
+
+        forgot_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"FORGOT PASSWORD", Toast.LENGTH_SHORT).show();
+                //TODO: REDIRECT TO FORGOT PASSWORD PAGE
             }
         });
     }
