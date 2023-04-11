22aab95083565cae77c47a82d7d928c98a98fa01
Jake <jaykegamer12@gmail.com>
2023-04-11 18:57:22 -0500

    login.java

--- app/src/main/java/com/example/unimeet3/Login.java
+++ app/src/main/java/com/example/unimeet3/Login.java
@@ -2,12 +2,64 @@ package com.example.unimeet3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    TextInputEditText editTextEmail, editTextPassword;
+    Button buttonLogin;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    TextView textViewRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editTextEmail = findViewById(R.id.login);
+        editTextPassword = findViewById(R.id.password);
+        mAuth = FirebaseAuth.getInstance();
+        buttonLogin = findViewById(R.id.login_button);
+        progressBar = findViewById(R.id.progress_bar);
+        textViewRegister = findViewById(R.id.newuser);
+
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Registration.class); //creates the new activity (aka new page)
+                startActivity(intent); //start
+                finish();
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if(TextUtils.isEmpty(email)) {
+                    Toast.makeText(Login.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password)) {
+                    Toast.makeText(Login.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
     }
 }
