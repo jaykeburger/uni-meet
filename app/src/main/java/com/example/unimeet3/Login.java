bd92cb5c63476369fe197e3830300b579fdac5cd
Jake <jaykegamer12@gmail.com>
2023-04-11 18:09:40 -0500

    updated manifests and gradle files

---
+++ app/src/main/java/com/example/unimeet3/Login.java
@@ -0,0 +1,13 @@
+package com.example.unimeet3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
