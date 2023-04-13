bf46a88a2513f4bf633ab1855c30509254c27473
Jake <jaykegamer12@gmail.com>
2023-04-13 01:38:22 -0500

    MainActivity.java changes

--- app/src/main/java/com/example/unimeet3/MainActivity.java
+++ app/src/main/java/com/example/unimeet3/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,19 +23,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.z_activity_main);
-        final EditText edit_name = findViewById(R.id.bruh);
-        final EditText edit_position = findViewById(R.id.bruh);
-        Button button = findViewById(R.id.bruh);
+        setContentView(R.layout.profile_setup);
+        final EditText name = findViewById(R.id.name);
+        final EditText age = findViewById(R.id.age);
+        final EditText pic = findViewById(R.id.pic);
+        final EditText major = findViewById(R.id.major);
+        final EditText minor = findViewById(R.id.minor);
+        final EditText bio = findViewById(R.id.bio);
+        final EditText org1 = findViewById(R.id.org1);
+        final EditText org2 = findViewById(R.id.org2);
+        final EditText org3 = findViewById(R.id.org3);
+        final EditText class1 = findViewById(R.id.class1);
+        final EditText class2 = findViewById(R.id.class2);
+        final EditText class3 = findViewById(R.id.class3);
+        final EditText class4 = findViewById(R.id.class4);
+        final EditText class5 = findViewById(R.id.class5);
+        final EditText class6 = findViewById(R.id.class6);
+        final EditText hobby1 = findViewById(R.id.hobby1);
+        final EditText hobby2 = findViewById(R.id.hobby2);
+        final EditText hobby3 = findViewById(R.id.hobby3);
+        final EditText hobby4 = findViewById(R.id.hobby4);
+        final EditText hobby5 = findViewById(R.id.hobby5);
+        final EditText hobby6 = findViewById(R.id.hobby6);
+        Button update_button = findViewById(R.id.upload_button);
 
         DAOProfileFeed dao = new DAOProfileFeed();
-        button.setOnClickListener( v-> {
+        update_button.setOnClickListener( v-> {
 
             String[] x = new String[6];
             String[] y = new String[6];
             String[] z = new String[6];
 
-            ProfileFeed profileFeed = new ProfileFeed(edit_name.getText().toString(), edit_position.getText().toString(), "", "","", "", x, y, z);
+            ProfileFeed profileFeed = new ProfileFeed(name.getText().toString(), age.getText().toString(), "", "","", "", x, y, z);
 
             dao.add(profileFeed).addOnSuccessListener(suc->
             {
